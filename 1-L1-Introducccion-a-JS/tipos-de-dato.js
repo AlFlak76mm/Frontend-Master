a1c2c40003b467ed93e64baf91554a0bb9772842
Alfredo <josealfredoperezdominguez@gmail.com>
2026-01-27 20:31:54 -0600

    Se agregaron ejemplos de campus

---
+++ 1-L1-Introducccion-a-JS/tipos-de-dato.js
@@ -0,0 +1,24 @@
+
+let numeroPositivo = 42;
+console.log(typeof numeroPositivo);
+
+console.log(typeof 'Veinticinco');
+
+console.log(typeof -666);
+
+let booleanTrue = true;
+console.log(typeof booleanTrue);
+
+let numeroCero = 0;
+console.log(typeof numeroCero);
+
+let comillaBienSola = "'";
+console.log(typeof comillaBienSola);
+
+let nullStuff = null;
+console.log(typeof nullStuff);
+
+let undefinedStuff
+console.log(typeof undefinedStuff);
+
+console.log(typeof FALSE);
