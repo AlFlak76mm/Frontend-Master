8f598cdf676f8894596f90188f46dbb779a7fd7d
Alfredo <josealfredoperezdominguez@gmail.com>
2026-01-27 20:23:14 -0600

    Archivos base | Progreso en clase

---
+++ 1-L1-Introducccion-a-JS/main.js
@@ -0,0 +1,70 @@
+
+//Comentario aca bien aftercatabath
+
+//Para declarar un valor se escribe al principio "let"
+
+/*
+Tipo de datos
+
+Number: enteros y decimales
+
+*/
+
+let edad = 24;
+let precio = 199.99;
+
+console.log("Tipo de dato Number:");
+console.log(edad);
+console.log(precio);
+console.log(typeof edad);
+console.log(typeof precio);
+
+//String
+let nombre = "Gato";
+let saludo = "Hola mundo";
+console.log("Tipo de dato String:");
+console.log(nombre);
+console.log(saludo);
+console.log(typeof nombre);
+console.log(typeof saludo);
+
+//Boolean
+let esMayorDeEdad = true;
+let tienePermiso = false;
+console.log(esMayorDeEdad)
+console.log(tienePermiso);
+console.log(typeof esMayorDeEdad);
+
+//Undefined
+let telefono;
+console.log("Tipo de dato Undefined:");
+console.log(telefono);
+console.log(typeof telefono);
+
+//Null
+console.log("Tipo de dato null:");
+let direccion = null;
+console.log(direccion);
+console.log(typeof direccion);
+
+//Object
+let persona = {
+    nombre: "Diego",
+    edad: "22",
+    ciudad: "CMDX"
+};
+console.log("Tipo de dato Object");
+console.log(typeof persona);
+console.log(persona);
+
+//Symbol (identificador unico)
+let id = Symbol("id");
+console.log("Tipo de dato Symbol:");
+console.log(id);
+console.log(typeof id);
+
+
+
+/*
+Otro comentario largo
+*/
