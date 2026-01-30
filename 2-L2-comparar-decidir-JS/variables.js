d27e2307a8dac67c41978ee7a0508c62c218a51f
Alfredo <josealfredoperezdominguez@gmail.com>
2026-01-29 19:10:38 -0600

    Enlaze script; var; let; const; suma; resta; comparacion simple y estricta

---
+++ 2-L2-comparar-decidir-JS/variables.js
@@ -0,0 +1,46 @@
+
+//Codigo visto en clase
+
+//var permite redeclarar la variable sobreescribiendo los valores previamente asignados
+var color = 'morado';
+var color = 'azul';
+var color = 'verde';
+
+console.log(color);
+
+
+//let asigna un valor a una variable el cual puede cambiar pero no volver a ser declarado
+let mascota = 'perro';
+let mascota1 = 'gato'
+
+
+//const asigna un valor no modificable
+const numero = 23;
+
+
+//suma y resta de variables
+
+let num1 = 3;
+let num2 = 5;
+
+let suma = num1 + num2;
+let resta = num2- num1;
+
+
+console.log(suma);
+console.log(15+15);
+console.log(resta);
+
+console.log(num1*num2);
+console.log(num2/num1);
+
+
+//comparacion simple
+console.log(num1 == num2);
+console.log(10 == 10);
+
+console.log(12 == '12');
+
+
+//comparacion estricta
+console.log(23 === "23");
