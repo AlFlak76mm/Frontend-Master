3c16304bba07686c4c3c247c44d0dc30f68003df
Alfredo <josealfredoperezdominguez@gmail.com>
2026-02-10 18:41:04 -0600

    Leccion 5

--- 4-L4-funciones-JS/funciones.js
+++ 4-L4-funciones-JS/funciones.js
@@ -60,14 +60,59 @@ console.log(mutis(123, 78));
 
 //Funciones anonimas | no requieren nombre
 //Callbacks
-setTimeout(function(){
+/* setTimeout(function(){
     console.log("esto se ejecuta al rato");
 }, 5000);
-
+ */
 
 //Cuando no usar esas cosas anonimas
 //Se tiene que reutilizar muchas veces
 //Tiene logica grande
 //Necesita ser testeada
 //Se necesita claridad
+
+function mascotas(a, b, c){
+    console.log("Nombres de laa mascotas: ", a+b+c)
+}
+mascotas('Perro', "Gato", "Nino");
+
+//3 funciones | Usar 2 ejemplos
+//Area de un triangulo
+//Verificar si un numero es primo
+//Generar numeros de fibonacci
+
+const areaTriangulo = (b, h) => {
+    return ((b*h)/2);
+}
+console.log(areaTriangulo(12, 4));
+console.log(areaTriangulo(26, 43));
+
+const nPrimo = (v1) => {
+    if (v1 % 2 === 1){
+        console.log(v1 + " es primo");
+    } else{
+        console.log(v1 + " no es primo");
+    }
+}
+nPrimo(8);
+nPrimo(27);
+
+const nFibonacci = (n) => {
+    let a = 1;
+    let b = 1;
+  for (let i = 3; i <= n; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+  }
+  return b;
+}
+
+console.log(nFibonacci(7));
+
+
+function fib(n) {
+  return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+console.log(fib(7));
 
