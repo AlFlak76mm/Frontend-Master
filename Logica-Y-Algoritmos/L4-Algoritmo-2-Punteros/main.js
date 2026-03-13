fbe37deef74d88b2b9d6c0df238864c7826567d8
Alfredo <josealfredoperezdominguez@gmail.com>
2026-03-12 21:13:33 -0500

    Repaso en clase

--- Logica-Y-Algoritmos/L4-Algoritmo-2-Punteros/main.js
+++ Logica-Y-Algoritmos/L4-Algoritmo-2-Punteros/main.js
@@ -35,3 +35,49 @@ function encontrarPar(arr, objetivo){
 let numeros = [2, 4, 7, 10, 11, 15, 20, 40, 80]
 let resultado = encontrarPar(numeros, 9);
 console.log(resultado);
+
+
+//Ejercicios; crear una funcion que revice si es palindromo
+// anita lava la tina
+//radar
+//palabra extra aftercatabath
+
+
+//Mi cosa fallida
+function palindromoPalindroso(cadenaTexto){
+    const letras = textoTextoso1.split("");
+}
+
+
+
+const textoTextoso1 = "anita lava la tina";
+const textoTextoso2 = "radar";
+const textoTextoso3 = "oso";
+
+
+//El realizo por la profe
+function esPalindromo(str){
+
+    str = str.toLowerCase();
+    let izq = 0;
+    let der = str.length - 1;
+
+    while (izq < der){
+        //comparamos cada caracter del lado izq con el caracter del lado derecho
+        if(str[izq] !== str[der]){
+            return false;
+        }
+        //Si coinciden movemos los punteros de izq a derecha
+        izq++;
+        //El derecho retrocede hacia la izq
+        der--;
+    }
+    //Encontramos el que si es palindromo
+    return true;
+}
+
+let palabra = 'radar';
+console.log(esPalindromo(palabra));
+
+let palabra2 = 'anita lava la tina';
+console.log(esPalindromo(palabra2));
