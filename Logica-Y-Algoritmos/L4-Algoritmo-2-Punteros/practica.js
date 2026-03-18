ecc6b4d69ce567d1d74bcd2b9f4bfe6fa1987994
Alfredo <josealfredoperezdominguez@gmail.com>
2026-03-17 19:39:56 -0500

    Estructura proporcionada para la practica

---
+++ Logica-Y-Algoritmos/L4-Algoritmo-2-Punteros/practica.js
@@ -0,0 +1,15 @@
+
+//Re
+const invitados = ["Ana", "Carlos", "Cecilia", "Daniel", "Diana", "Eduardo"];
+
+function encontrarPareja(arr) {
+    let inicio = 0;
+    let siguiente = 1;
+
+    while (siguiente < arr.length) {
+
+
+    }
+    return null;
+}
+console.log(encontrarPareja(invitados));
