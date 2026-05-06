3cef52f82a9e3330ae569f7b5c26ac3c6775920d
Alfredo <josealfredoperezdominguez@gmail.com>
2026-05-05 21:26:16 -0500

    Practica 1: completada

--- Desarrollo-Avanzado-en-Javascript/L1-Event-Loopp-y-Asincronicidad/practica.js
+++ Desarrollo-Avanzado-en-Javascript/L1-Event-Loopp-y-Asincronicidad/practica.js
@@ -2,4 +2,43 @@
 const orderList = document.getElementById('orderList');
 const addOrderBtn = document.getElementById('addOrderBtn');
 
-let order
+let orderId = 1; //Esta variable ayuda a identificar los pedidos
+
+addOrderBtn.addEventListener('click', () =>{
+    const order = {id: orderId++, status: 'En proceso'};
+    addOrder(order);
+    processOrder(order)
+        .then(result => {
+            console.log(result);
+            return updateOrderStatus(order, "Completada");
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
+
+function addOrder(order){
+    const listItem = document.createElement('li');
+    listItem.id = `order-${order.id}`;
+    listItem.textContent = `Pedido #${order.id}: ${order.status}`;
+    orderList.appendChild(listItem);
+}
+
+function updateOrderStatus(order, status){
+    const listItem = document.getElementById(`order-${order.id}`);
+    if(listItem){
+        listItem.textContent = `Pedido #${order.id}: ${status}`;
+    }
+}
+
+async function processOrder(order){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (order) {
+                resolve("Orden completa");
+            } else{
+                reject("No se registro una orden.");
+            }
+        }, 2000);
+    });
+}
