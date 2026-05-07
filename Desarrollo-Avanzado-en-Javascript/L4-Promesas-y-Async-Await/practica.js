a88d6404a986eaf2aae85b7486a82a118fff1af2
Alfredo <josealfredoperezdominguez@gmail.com>
2026-05-06 20:56:42 -0500

    Practica L4-Promesas-Y-Async-Await terminada

--- Desarrollo-Avanzado-en-Javascript/L4-Promesas-y-Async-Await/practica.js
+++ Desarrollo-Avanzado-en-Javascript/L4-Promesas-y-Async-Await/practica.js
@@ -9,6 +9,11 @@ function verificarDisponibilidad(mesasSolicitadas) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       // Completa la lógica aquí: Si hay suficientes mesas disponibles, resuelve la promesa, 
+      if (mesasSolicitadas <= mesasDisponibles){
+        resolve("Hay suficientes mesas disponibles");
+      }else{
+        reject("No hay suficientes mesas disponibles");
+      }
       // de lo contrario, recházala con un mensaje adecuado.
     }, 2000);  // Simula un retraso en la verificación (2 segundos)
   });
@@ -18,8 +23,16 @@ function verificarDisponibilidad(mesasSolicitadas) {
 function enviarConfirmacionReserva(nombreCliente) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      // Completa la lógica aquí: Simula un envío de correo. Usa Math.random() 
-      // para simular si el correo se envió correctamente o si ocurrió un error.
+        // Completa la lógica aquí: Simula un envío de correo. Usa Math.random() 
+        let valorRandom = Math.floor(Math.random() * 10) + 1;
+        
+        // para simular si el correo se envió correctamente o si ocurrió un error.
+        if (valorRandom <= 8){
+            resolve(`Correo enviado correctamente a: ${nombreCliente}`);
+        } else{
+            reject("Error al enviar el correo");
+        }
+    
     }, 1500);  // Simula el envío de un correo (1.5 segundos)
   });
 }
@@ -29,7 +42,15 @@ async function hacerReserva(nombreCliente, mesasSolicitadas) {
   try {
     console.log("Verificando disponibilidad de mesas...");
     const disponibilidad = await verificarDisponibilidad(mesasSolicitadas);  // Llama a la función de verificación
-    
+    console.log(disponibilidad);
+
+    if(disponibilidad){
+    console.log("Enviando confirmación de reserva...");
+    const confirmacion = await enviarConfirmacionReserva(nombreCliente);
+    console.log(confirmacion);
+    }
+
+
     // Completa el flujo aquí: Si hay mesas disponibles, llama a la función para enviar la confirmación.
     // Si no hay mesas disponibles o si ocurre un error, captura el error.
   } catch (error) {
@@ -39,3 +60,4 @@ async function hacerReserva(nombreCliente, mesasSolicitadas) {
 
 // Llamada de prueba
 hacerReserva("Juan Pérez", 3);  // Intenta hacer una reserva para 3 personas
+//hacerReserva("Jose Pérez", 7);
